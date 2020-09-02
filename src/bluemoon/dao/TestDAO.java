9b8e317ccfa39a568e844619f19a8194fee15e92
merpi3939 <zlwear@naver.com>
2020-09-02 19:31:50 +0900

    dao

---
+++ src/bluemoon/dao/TestDAO.java
@@ -0,0 +1,8 @@
+package bluemoon.dao;
+
+public class TestDAO {
+	public static void main(String[] args) {
+		System.out.println("test dao ¿‘¥œ¥Ÿ.");
+	}
+
+}
--- src/finalproject/dao/testDAO.java
+++
@@ -1,8 +0,0 @@
-package finalproject.dao;
-
-public class testDAO {
-	public static void main(String[] args) {
-		System.out.println("æ»≥Á«œººø‰.");
-	}
-
-}
