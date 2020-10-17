77465188ba62982cf17fe7d5288f75fe43516bc3
black <black@DESKTOP-V9F0VPA>
2020-10-18 02:41:19 +0900

    오션컨트롤러수정

--- src/main/java/site/bluemoon/controller/oceanController.java
+++ src/main/java/site/bluemoon/controller/oceanController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class oceanController {
-	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String waterPark() {
 		return "main";
 	}
