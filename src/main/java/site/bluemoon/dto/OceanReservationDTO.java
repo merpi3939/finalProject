3e8ecef8104a4726ac06dec95a919496a173b75d
한결 <@HanGyeol>
2020-10-17 20:30:29 +0900

    waterpark DTO

---
+++ src/main/java/site/bluemoon/dto/OceanReservationDTO.java
@@ -0,0 +1,129 @@
+package site.bluemoon.dto;
+
+public class OceanReservationDTO {
+	private int rsNo;
+	private int rsUno;
+	private String rsName;
+	private String rsEmail;
+	private String rsPhone;
+	private String rsDate;
+	private String rsUserdate;
+	private String rsTicket;
+	private int rsAdult;
+	private int rsChild;
+	private String rsState;
+	private int rsPrice;
+	private String rsOption;
+
+	public OceanReservationDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getRsNo() {
+		return rsNo;
+	}
+
+	public void setRsNo(int rsNo) {
+		this.rsNo = rsNo;
+	}
+
+	public int getRsUno() {
+		return rsUno;
+	}
+
+	public void setRsUno(int rsUno) {
+		this.rsUno = rsUno;
+	}
+
+	public String getRsName() {
+		return rsName;
+	}
+
+	public void setRsName(String rsName) {
+		this.rsName = rsName;
+	}
+
+	public String getRsEmail() {
+		return rsEmail;
+	}
+
+	public void setRsEmail(String rsEmail) {
+		this.rsEmail = rsEmail;
+	}
+
+	public String getRsPhone() {
+		return rsPhone;
+	}
+
+	public void setRsPhone(String rsPhone) {
+		this.rsPhone = rsPhone;
+	}
+
+	public String getRsDate() {
+		return rsDate;
+	}
+
+	public void setRsDate(String rsDate) {
+		this.rsDate = rsDate;
+	}
+
+	public String getRsUserdate() {
+		return rsUserdate;
+	}
+
+	public void setRsUserdate(String rsUserdate) {
+		this.rsUserdate = rsUserdate;
+	}
+
+	public String getRsTicket() {
+		return rsTicket;
+	}
+
+	public void setRsTicket(String rsTicket) {
+		this.rsTicket = rsTicket;
+	}
+
+	public int getRsAdult() {
+		return rsAdult;
+	}
+
+	public void setRsAdult(int rsAdult) {
+		this.rsAdult = rsAdult;
+	}
+
+	public int getRsChild() {
+		return rsChild;
+	}
+
+	public void setRsChild(int rsChild) {
+		this.rsChild = rsChild;
+	}
+
+	public String getRsState() {
+		return rsState;
+	}
+
+	public void setRsState(String rsState) {
+		this.rsState = rsState;
+	}
+
+	public int getRsPrice() {
+		return rsPrice;
+	}
+
+	public void setRsPrice(int rsPrice) {
+		this.rsPrice = rsPrice;
+	}
+
+	public String getRsOption() {
+		return rsOption;
+	}
+
+	public void setRsOption(String rsOption) {
+		this.rsOption = rsOption;
+	}
+	
+	
+	
+}//
+
