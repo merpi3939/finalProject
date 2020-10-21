84dff568d27416b0c4c3efc92b63d8f94feefe14
한결 <@HanGyeol>
2020-10-21 22:49:33 +0900

    Ocean DTO

--- src/main/java/site/bluemoon/dto/OceanReservationDTO.java
+++ src/main/java/site/bluemoon/dto/OceanReservationDTO.java
@@ -1,10 +1,7 @@
 package site.bluemoon.dto;
 
-<<<<<<< HEAD
 import javax.validation.constraints.Pattern;
 
-=======
->>>>>>> refs/remotes/origin/master
 /*
   이름         널        유형            
 ---------- -------- ------------- 
