d8e232c8077b59a88e90ca07f4fc1acb884bdd63
black <black@DESKTOP-V9F0VPA>
2020-10-23 18:54:00 +0900

    ㅁㄴㅇ

--- src/main/java/site/bluemoon/controller/UserController.java
+++ src/main/java/site/bluemoon/controller/UserController.java
@@ -1,8 +1,6 @@
 package site.bluemoon.controller;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +11,6 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
