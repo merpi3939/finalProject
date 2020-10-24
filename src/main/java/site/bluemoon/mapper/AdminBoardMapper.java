14ec411858fec7f348ab56be09ea8211867bc4ff
Soyoung Pyo <zlwear@naver.com>
2020-10-24 19:03:23 +0900

    주석 풀기

--- src/main/java/site/bluemoon/mapper/AdminBoardMapper.java
+++ src/main/java/site/bluemoon/mapper/AdminBoardMapper.java
@@ -5,12 +5,13 @@ import java.util.Map;
 
 import site.bluemoon.dto.InfoBoard;
 
+
 public interface AdminBoardMapper {
 	//INFO
-	/*
+	
 	List<InfoBoard> selectInfoList(Map<String, Object> map);
 	InfoBoard selectInfo(int infoNo);
 	int updateInfo(InfoBoard infoBoard);
 	int updateInfoState(InfoBoard infoBoard);
-	*/
+	
 }
