92af826f10b96fc0047efb769980568d609a3378
bbak0105 <66405572+bbak0105@users.noreply.github.com>
2020-10-26 14:58:43 +0900

    asdasd

--- src/main/java/site/bluemoon/controller/UserExceptionController.java
+++ src/main/java/site/bluemoon/controller/UserExceptionController.java
@@ -22,11 +22,11 @@ public class UserExceptionController {
 		model.addAttribute("message", exception.getMessage());
 		return "error/error";
 	}
-	/*
+	
 	@ExceptionHandler(Exception.class)
 	public String exceptionHandler(Exception exception, Model model) {
 		model.addAttribute("message", exception.getMessage());
 		return "error/error";
 	}
-	*/
+	
 }
